553d8519d80483dd9a9f3ac694c38d860180b6e4
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-04-05 21:10:05 -0700

    add callbacks + get # tweets

--- src/data/tweets.js
+++ src/data/tweets.js
@@ -1,7 +1,7 @@
 var api_root = "http://c2952e3a-4e6a01aaa4c0.my.apitools.com/1.1/";
 
 var Tweets  = {
-	getTrendingTopics: function(){
+	getTrendingTopics: function(cb){
 		// var api_url = "https://api.twitter.com/1.1/trends/place.json?id=2487956"; //SF id
 		var api_url = api_root+"trends/place.json?id=2487956"
 		$.ajax({
@@ -9,8 +9,7 @@ var Tweets  = {
 	        url: api_url,
 	        dataType: "jsonp",
 	        success: function (response, status, xhr) {
-              console.log(response[0].trends);
-              return response[0].trends;
+              cb(response[0].trends);
 	        },
 	        error: function (xhr, err) {
 	          console.log(xhr);
@@ -39,4 +38,99 @@ var Tweets  = {
 		});
 
 	},
+	getTweetsByTag: function(query,nb,cb){
+		var api_url = api_root+"search/tweets.json";
+		api_url+="?q="+encodeURIComponent(query);
+		api_url+="&count="+nb;
+		$.ajax({
+	        type: "GET",
+	        url: api_url,
+	        dataType: "jsonp",
+	        success: function (response, status, xhr) {
+              cb(response.statuses)
+	        },
+	        error: function (xhr, err) {
+	          console.log(xhr);
+	          console.log(xhr.statusText);
+	        }
+		});
+	},
+	getThirtyTweets: function(cb,nb){
+		var all_tweets=[];
+		var allDone = barrier(9,function(){
+			console.log("ALL TWEETS", all_tweets);
+			cb(all_tweets);
+		});
+
+		// Tweets.getTrendingTopics(function(data){
+			//FAKE DATA
+			var data =[{
+			name: "#BuyBuckWildOniTunes",
+			query: "%23BuyBuckWildOniTunes",
+			url: "http://twitter.com/search?q=%23BuyBuckWildOniTunes",
+			promoted_content: null
+			},
+			{
+			name: "#WWEHOF",
+			query: "%23WWEHOF",
+			url: "http://twitter.com/search?q=%23WWEHOF",
+			promoted_content: null
+			},
+			{
+			name: "Aaron Harrison",
+			query: "%22Aaron+Harrison%22",
+			url: "http://twitter.com/search?q=%22Aaron+Harrison%22",
+			promoted_content: null
+			},
+			{
+			name: "#FinalFour",
+			query: "%23FinalFour",
+			url: "http://twitter.com/search?q=%23FinalFour",
+			promoted_content: null
+			},
+			{
+			name: "#PixelRaceGame",
+			query: "%23PixelRaceGame",
+			url: "http://twitter.com/search?q=%23PixelRaceGame",
+			promoted_content: null
+			},
+			{
+			name: "Uconn vs Kentucky",
+			query: "%22Uconn+vs+Kentucky%22",
+			url: "http://twitter.com/search?q=%22Uconn+vs+Kentucky%22",
+			promoted_content: null
+			},
+			{
+			name: "Wisconsin",
+			query: "Wisconsin",
+			url: "http://twitter.com/search?q=Wisconsin",
+			promoted_content: null
+			},
+			{
+			name: "#MarchMadness",
+			query: "%23MarchMadness",
+			url: "http://twitter.com/search?q=%23MarchMadness",
+			promoted_content: null
+			},
+			{
+			name: "Captain America",
+			query: "%22Captain+America%22",
+			url: "http://twitter.com/search?q=%22Captain+America%22",
+			promoted_content: null
+			},
+			{
+			name: "Florida",
+			query: "Florida",
+			url: "http://twitter.com/search?q=Florida",
+			promoted_content: null
+			}];
+			var trending_topics = data;
+			console.log("ahahahaha", data);
+			for(var i=0;i<trending_topics.length-1;i++){
+				var tweets =Tweets.getTweetsByTag(trending_topics[i],nb,function(tweets_data){
+					all_tweets= all_tweets.concat(tweets_data);
+					allDone();
+				});
+			}
+	}
 }
